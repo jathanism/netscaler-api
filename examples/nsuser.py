c369a900a0ba249fb7ee30e3c1b92f560a93091d
Jathan McCollum <jathan+bitbucket@gmail.com>
2010-03-02 23:02:33 -0800

    0.2 commit, see CHANGELOG for details

---
+++ examples/nsuser.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import netscaler
+import os
+
+class IllegalName(netscaler.InteractionError): pass
+
+class UserAdmin(netscaler.API):
+    def is_safe(self, username):
+        """Returns False for names containing 'root' or starting with 'ns'."""
+        if 'root' in username or username.startswith('ns'):
+            return False
+        return True
+
+    def add_user(self, username, password):
+        """Custom user adder that won't allow unsafe names"""
+        if not self.is_safe(username):
+            raise IllegalName(username)
+
+        try:
+            resp = self.run("addsystemuser", **{'username': username, 'password': password})
+            return True
+        except netscaler.InteractionError, err:
+            return False
+
+    def del_user(self, username):
+        """Custom user remover that protects usernames"""
+        if not self.is_safe(username):
+            raise IllegalName(username)
+
+        try:
+            resp = self.run("rmsystemuser", **{'username': username})
+            return True
+        except netscaler.InteractionError, err:
+            return False
+
+    def user_exists(self, username):
+        """Returns True if user exists."""
+        try:
+            resp = self.run("getsystemuser", **{'username': username} )
+            return True
+        except netscaler.InteractionError, err:
+            return False
+
+
+if __name__ == '__main__':
+
+    netscaler.DEBUG = True
+
+    cwd = os.getcwd()
+
+
+    wsdl_url = 'file://%s/NSUserAdmin.wsdl' % cwd
+    username = password = 'nsroot'
+    host = 'netscaler'
+
+    api = UserAdmin(host, username=username, password=password, wsdl_url=wsdl_url, cache=None)
+
+    api.login()
+    print 'logged in:', api.logged_in
+    print 'autosave? ', api.autosave
+    print
+
+    users = ('jathan', 'dynasty', 'john')
+
+    for user in users:
+        print 'checking', user
+        if api.user_exists(user):
+            print user, 'exists.'
+            print 'deleting', user
+            api.del_user(user)
+            print
+
+        else:
+            # add a user with matching password
+            print user, 'no exists.'
+            if api.add_user(user, user):
+                print user, 'added!'
+                print
+            
+
---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, Command
+except ImportError:
+    from distutils.core import setup, Command
+import os
+import sys
+
+from netscaler import __version__
+
+if sys.version_info[:2] < (2, 4):
+    print "This package requires Python 2.4+. Sorry!"
+    sys.exit(-11)
+
+class CleanCommand(Command):
+    description = "cleans up non-package files. (dist, build, etc.)"
+    user_options = []
+    def initialize_options(self):
+        self.files = None
+    def finalize_options(self):
+        self.files = './build ./dist ./MANIFEST ./*.pyc examples/*.pyc ./*.egg-info'
+    def run(self):
+        print 'Cleaning: %s' % self.files
+        os.system('rm -rf ' + self.files)
+
+setup(
+    name = 'netscaler-api',
+    version = __version__,
+    url = 'http://bitbucket.org/jathanism/netscaler-api/',
+    download_url = 'http://bitbucket.org/jathanism/netscaler-api/downloads/',
+    license = 'BSD',
+    description = "NetScaler API is a Python interface for interacting with Citrix NetScaler application delivery controllers, utilizing the SOAP API to execute commands.",
+    author = 'Jathan McCollum',
+    author_email = 'jathan+bitbucket@gmail.com',
+    py_modules = ['netscaler'],
+    install_requires=['suds>=0.3.9'],
+    keywords = [
+            'Networking', 'Automation', 'library', 'Security', 'NetScaler', 'API', 'SOAP',
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Education :: Testing',
+        'Topic :: Internet',
+        'Topic :: Security',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+        'Topic :: System :: Networking :: Monitoring',
+        'Topic :: System :: Operating System',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+    cmdclass = {
+        'clean': CleanCommand,
+    }
+)
